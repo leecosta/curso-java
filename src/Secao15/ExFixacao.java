f4ecd660703cc357e7766d537ba98cf07a20474c
Letícia Costa Mancini <leticia.mancini@ibm.com>
2022-09-23 16:39:52 -0300

    aula 15

---
+++ src/Secao15/ExFixacao.java
@@ -0,0 +1,45 @@
+package Secao15;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Accounts;
+import exceptions.BusinessException;
+
+public class ExFixacao {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter account data");
+		System.out.print("Number: ");
+		int number = sc.nextInt();
+		System.out.print("Holder: ");
+		sc.nextLine();
+		String holder = sc.nextLine();
+		System.out.print("Initial balance: ");
+		double balance = sc.nextDouble();
+		System.out.print("Withdraw limit: ");
+		double withdrawLimit = sc.nextDouble();
+
+		Accounts acc = new Accounts(number, holder, balance, withdrawLimit);
+		
+		System.out.println();
+		System.out.print("Enter amount for withdraw: ");
+		double amount = sc.nextDouble();
+		
+		try {
+			acc.withdraw(amount);
+			System.out.printf("New balance: %.2f%n", acc.getBalance());
+		}
+		catch (BusinessException e) {
+			System.out.println("Withdraw error: " + e.getMessage());
+		}
+		
+		sc.close();
+
+	}
+
+}
